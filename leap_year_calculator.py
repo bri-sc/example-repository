63811f9bd8cf823acb378f6348a5fc2c4504e9a7
Rachel Tunnicliffe <37222223+rt17603@users.noreply.github.com>
2023-01-10 16:35:42 +0000

    Update leap_year_calculator.py
    Adding 400 year checking

--- leap_year_calculator.py
+++ leap_year_calculator.py
@@ -1,7 +1,9 @@
 
 year = 2012
 
-if year%4 == 0 and year%100 == 0:
+if year%4 == 0 and year%400 == 0:
+    leap_year = "Yes"
+elif year%4 == 0 and year%100 == 0:
     leap_year = "No"
 elif year%4 == 0:
     leap_year = "Yes"
